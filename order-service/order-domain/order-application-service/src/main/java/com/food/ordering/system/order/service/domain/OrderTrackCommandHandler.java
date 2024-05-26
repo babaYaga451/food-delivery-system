3c302a1a86f019688647cf97d71904445d98581b
sambitbehera <sambit.behera581@gmail.com>
2024-05-26 18:47:55 +0530

    Implemented OrderTrackCommandHandler

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,6 +2,10 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueObject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +13,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderTrackCommandHandler {
 
-  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+  private final OrderDataMapper orderDataMapper;
+  private final OrderRepository orderRepository;
 
+  public OrderTrackCommandHandler(OrderDataMapper orderDataMapper,
+      OrderRepository orderRepository) {
+    this.orderDataMapper = orderDataMapper;
+    this.orderRepository = orderRepository;
   }
 
+  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+    return orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()))
+        .map(orderDataMapper::orderToTrackOrderResponse)
+        .orElseThrow(() -> {
+          log.warn("Could not find order with id: {}", trackOrderQuery.getOrderTrackingId());
+          return new OrderNotFoundException("Could not find order with id: " +
+              trackOrderQuery.getOrderTrackingId());
+        });
+  }
 }
